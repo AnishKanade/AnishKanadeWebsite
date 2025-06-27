4a1b2530a987433438291d50c27d0878a47ed5ac
Anish kanade <anishkanade45@gmail.com>
2025-06-26 22:59:26 -0400

    Updates

---
+++ _app/immutable/chunks/home.23c5ae00.js
@@ -0,0 +1,9 @@
+import{P as s}from"./types.7ffc7bf1.js";import{g as a}from"./skills.b738ba6c.js";import{b as t}from"./paths.550b9e0b.js";const i=`${t}/Anish.JPG`,l="Home",r="Anish",c="Kanade",m="Cambridge, Massachusetts, United States",d=`
+  <p><strong>Hi there!</strong> My name is Anish Kanade, a Computer Science graduate from UMass Boston who builds data-driven, cloud-powered applications.</p>
+  <ul class="overview-list">
+    <li><strong>Serverless ETL on AWS:</strong> Reduced data latency by 50% while maintaining 98% consistency.</li>
+    <li><strong>React Frontend for HikeMate:</strong> Cut support tickets by 35% through responsive, reusable components.</li>
+    <li><strong>Django Research Platform:</strong> Deployed on EC2/S3, labeled 15,000+ samples via Mechanical Turk, and boosted emotion-recognition accuracy by 15% with CNN & LSTM models.</li>
+  </ul>
+  <p class="overview-footer">I thrive in collaborative, agile teams and love tackling complex challenges with new technologies.</p>
+`,p=[{platform:s.GitHub,link:"https://github.com/AnishKanade"},{platform:s.Linkedin,link:"https://www.linkedin.com/in/anish-kanade"},{platform:s.Email,link:"AnishKanade45@gmail.com"}],h=a("js","css","html","reactjs","sass","svelte","ts","python","pandas","matplotlib","scikit-learn","aws","docker","kubernetes","java","csharp","mysql","django");export{p as a,m as b,i as c,d,c as l,r as n,h as s,l as t};
--- _app/immutable/chunks/home.5242ed07.js
+++
@@ -1,5 +0,0 @@
-import{P as t}from"./types.7ffc7bf1.js";import{g as s}from"./skills.f8eda811.js";const n="Home",o="Anish",i="Kanade",l=`
-
-Hi there! I am currently a Computer Science undergraduate studying at the University of Massachusetts Boston, Class of 2025.
-I am passionate about software development, problem-solving, and exploring innovative tech solutions.
-I am always eager to learn new technologies and contribute to impactful projects. Check out more about me!`,m=[{platform:t.GitHub,link:"https://github.com/AnishKanade"},{platform:t.Linkedin,link:"https://www.linkedin.com/in/anish-kanade"},{platform:t.Email,link:"AnishKanade45@gmail.com"}],r=s("js","css","html","reactjs","sass","svelte","ts");export{m as a,l as d,i as l,o as n,r as s,n as t};
