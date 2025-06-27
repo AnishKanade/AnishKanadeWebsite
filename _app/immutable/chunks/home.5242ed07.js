c8b4bcd073e83f44688c3c3afcd3635cb5cac75d
Anish kanade <anishkanade45@gmail.com>
2025-06-26 22:29:47 -0400

    Updates

--- _app/immutable/chunks/home.5242ed07.js
+++ _app/immutable/chunks/home.5242ed07.js
@@ -1,4 +1,4 @@
-import{P as t}from"./types.7ffc7bf1.js";import{g as s}from"./skills.e6fb3b95.js";const n="Home",o="Anish",i="Kanade",l=`
+import{P as t}from"./types.7ffc7bf1.js";import{g as s}from"./skills.f8eda811.js";const n="Home",o="Anish",i="Kanade",l=`
 
 Hi there! I am currently a Computer Science undergraduate studying at the University of Massachusetts Boston, Class of 2025.
 I am passionate about software development, problem-solving, and exploring innovative tech solutions.
